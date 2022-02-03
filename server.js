36138900b47a7ddf202242e4a91852dde0d2f669
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-03 11:18:12 -0500

    index change

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 const path = require("path");
-
-const routes = require("./routes");
+const sequelize = require('./config/connection');
+const routes = require("./controllers");
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", routes);
