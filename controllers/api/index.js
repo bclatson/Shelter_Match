02f5681ae3283bdb1832c9ed031618501e140c59
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-03 19:41:36 -0500

    added test api to controllers/api/index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
+const { Dogs, Owner, Potential } = require('../models');
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+// const apiRoutes = require('./api');
+// const homeRoutes = require('./homeRoutes');
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+// router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
+
+router.get('/', async (req, res) => {
+    try {
+        const PotentialOwnerData = PotentialOwner.findAll();
+        return res.status(200).json(potentialOwnerData);
+    } catch(err) {
+        return res.status(500).json(err);
+    }
+});
 
 module.exports = router;
+
